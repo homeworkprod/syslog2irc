ecf4fe800460799e15f9b5be59f78bf75f04dd2b
Jochen Kupperschmidt <homework@nwsnet.de>
2013-06-02 23:41:58 +0200

    Added tests for the syslog message parser.

---
+++ test_syslog2irc.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for syslog2IRC
+====================
+
+Based on pytest_.
+
+Install it:
+
+.. code:: sh
+
+    $ pip install pytest
+
+Run the tests:
+
+.. code:: sh
+
+    $ py.test test_syslog2irc.py
+
+:Copyright: 2007-2013 `Jochen Kupperschmidt <http://homework.nwsnet.de/>`_
+:Date: 02-Jun-2013
+:License: MIT, see LICENSE for details.
+
+.. _pytest: http://pytest.org/
+"""
+
+from __future__ import unicode_literals
+from datetime import datetime
+
+import pytest
+
+from syslog2irc import SyslogMessage, SyslogMessageParser
+
+
+CURRENT_YEAR = datetime.today().year
+
+@pytest.mark.parametrize(
+    (
+        'data',
+        'expected_facility_id',
+        'expected_facility_name',
+        'expected_severity_id',
+        'expected_severity_name',
+        'expected_timestamp',
+        'expected_hostname',
+        'expected_message',
+    ), [
+        (
+            # Example 1 from RFC 3164.
+            '<34>Oct 11 22:14:15 mymachine su: \'su root\' failed for lonvick on /dev/pts/8',
+            4,
+            'security/authorization messages',
+            2,
+            'Critical',
+            datetime(CURRENT_YEAR, 10, 11, 22, 14, 15),
+            'mymachine',
+            'su: \'su root\' failed for lonvick on /dev/pts/8',
+        ),
+        (
+            # Example 2 from RFC 3164.
+            '<13>Feb  5 17:32:18 10.0.0.99 Use the BFG!',
+            1,
+            'user-level messages',
+            5,
+            'Notice',
+            datetime(CURRENT_YEAR, 2, 5, 17, 32, 18),
+            '10.0.0.99',
+            'Use the BFG!',
+        ),
+        (
+            # Example 3 from RFC 3164.
+            # Note that the HOSTNAME and MSG fields in this example are not
+            # consistent with what the RFC defines.
+            '<165>Aug 24 05:34:00 CST 1987 mymachine myproc[10]: %% It\'s time to make the do-nuts.  %%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%',
+            20,
+            'local use 4 (local4)',
+            5,
+            'Notice',
+            datetime(CURRENT_YEAR, 8, 24, 5, 34, 0),
+            'CST',
+            '1987 mymachine myproc[10]: %% It\'s time to make the do-nuts.  %%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%',
+        ),
+            # Example 4 from RFC 3164.
+            # This cannot be parsed because the format of the TIMESTAMP field
+            # is invalid.
+            #'<0>1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That\'s All Folks!',
+        (
+            # Example 5 from RFC 3164.
+            '<0>Oct 22 10:52:12 scapegoat 1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That\'s All Folks!',
+            0,
+            'kernel messages',
+            0,
+            'Emergency',
+            datetime(CURRENT_YEAR, 10, 22, 10, 52, 12),
+            'scapegoat',
+            '1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That\'s All Folks!',
+        ),
+    ]
+)
+def test_syslog_message_parser(
+        data,
+        expected_facility_id,
+        expected_facility_name,
+        expected_severity_id,
+        expected_severity_name,
+        expected_timestamp,
+        expected_hostname,
+        expected_message):
+    actual = SyslogMessageParser.parse(data)
+    assert actual.facility_id == expected_facility_id
+    assert actual.facility_name == expected_facility_name
+    assert actual.severity_id == expected_severity_id
+    assert actual.severity_name == expected_severity_name
+    assert actual.timestamp == expected_timestamp
+    assert actual.hostname == expected_hostname
+    assert actual.message == expected_message
