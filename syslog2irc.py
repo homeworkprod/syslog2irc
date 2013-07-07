2531ab57eecf85371899483b3dda7edc1a821836
Jochen Kupperschmidt <homework@nwsnet.de>
2013-07-08 01:04:47 +0200

    Extracted, rearranged, and documented methods.

--- syslog2irc.py
+++ syslog2irc.py
@@ -58,6 +58,7 @@ DEFAULT_SYSLOG_PORT = 514
 
 
 class SyslogMessageParser(object):
+    """Parse syslog messages."""
 
     @classmethod
     def parse(cls, data):
@@ -167,7 +168,8 @@ class SyslogRequestHandler(BaseRequestHandler):
 
     def handle(self):
         try:
-            msg = SyslogMessageParser.parse(self.request[0].strip())
+            data = self.request[0].strip()
+            msg = SyslogMessageParser.parse(data)
         except ValueError:
             msg = 'Invalid message.'
         else:
@@ -187,11 +189,12 @@ class SyslogReceiveServer(ThreadingUDPServer):
 # IRC bot stuff
 
 
-class SyslogBot(SingleServerIRCBot):
+class IrcBot(SingleServerIRCBot):
+    """An IRC bot to forward syslog messages to the configured channels."""
 
-    def __init__(self, hostAndPort, channel_list, nickname, realname):
-        print 'Connecting to IRC server %s:%d ...' % hostAndPort
-        SingleServerIRCBot.__init__(self, [hostAndPort], nickname, realname)
+    def __init__(self, host_and_port, channel_list, nickname, realname):
+        print 'Connecting to IRC server %s:%d ...' % host_and_port
+        SingleServerIRCBot.__init__(self, [host_and_port], nickname, realname)
         self.channel_list = channel_list
 
     def on_welcome(self, conn, event):
@@ -234,18 +237,8 @@ class SyslogBot(SingleServerIRCBot):
 # ---------------------------------------------------------------- #
 
 
-def process_queue(announce_callback, queue, delay=2):
-    """Process received messages in queue."""
-    while True:
-        sleep(delay)
-        try:
-            addr, msg = queue.get()
-        except Empty:
-            continue
-        announce_callback('%s:%d ' % addr + str(msg))
-
 def parse_args():
-    """Parse command line arguments."""
+    """Setup and apply the command line arguments parser."""
 
     HostAndPort = namedtuple('HostAndPort', ['host', 'port'])
 
@@ -292,26 +285,53 @@ def parse_args():
 
     return parser.parse_args()
 
-if __name__ == '__main__':
-    args = parse_args()
+def start_announcer(args, irc_channels):
+    """If IRC output is enabled, configure the IRC bot and start it in a
+    separate thread. If it is disabled, just write to STDOUT.
 
-    # Set IRC connection parameters.
-    irc_channels = [('#examplechannel', 'secret')]
-
+    Return the announce callback.
+    """
     if args.irc_enabled:
         # Prepare and start IRC bot.
-        bot = SyslogBot(args.irc_server, irc_channels, args.irc_nickname,
+        bot = IrcBot(args.irc_server, irc_channels, args.irc_nickname,
             args.irc_realname)
         Thread(target=bot.start).start()
-        announce = bot.say
+        return bot.say
     else:
         # Just display messages on STDOUT.
-        print 'IRC output is disabled.'
-        def announce(s):
-            print s
+        print 'IRC output is disabled; writing to STDOUT instead.'
+        return print
 
-    # Prepare and start syslog message receiver.
+def start_syslog_message_receiver(args):
+    """Prepare the server to receive syslog messages on the configured port
+    and start it in a separate thread.
+
+    Return the receiver queue.
+    """
     receiver = SyslogReceiveServer(args.syslog_port)
     Thread(target=receiver.serve_forever).start()
+    return receiver.queue
 
-    process_queue(announce, receiver.queue)
+def process_queue(announce_callback, queue, delay=2):
+    """Process received messages in queue."""
+    while True:
+        sleep(delay)
+        try:
+            addr, msg = queue.get()
+        except Empty:
+            continue
+        announce_callback('%s:%d ' % addr + str(msg))
+
+def main(irc_channels):
+    args = parse_args()
+    announce_callback = start_announcer(args, irc_channels)
+    queue = start_syslog_message_receiver(args)
+    process_queue(announce_callback, queue)
+
+if __name__ == '__main__':
+    # Configure IRC channels to join.
+    IRC_CHANNELS = [
+        ('#examplechannel', 'secret'),
+    ]
+
+    main(IRC_CHANNELS)
