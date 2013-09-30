73df951e7043daecd2f48325bfad16831f190d57
Jochen Kupperschmidt <homework@nwsnet.de>
2013-09-30 02:13:44 +0200

    Use tox to run the tests against mulitple versions of Python.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/env/bin python
+
+from setuptools import setup
+
+
+setup(
+    name='syslog2IRC',
+    version='0.5',
+    description='A proxy to forward syslog messages to IRC',
+    author='Jochen Kupperschmidt',
+    author_email='homework@nwsnet.de',
+    url='http://homework.nwsnet.de/releases/c474/#syslog2irc',
+    install_requires=[
+        'blinker >= 1.3',
+        'irc >= 8.3.1',
+    ],
+)
