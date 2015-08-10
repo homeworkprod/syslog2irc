38e562b9e78e349544d1264387003043194f5285
Jochen Kupperschmidt <homework@nwsnet.de>
2015-08-10 13:44:00 +0200

    Removed local syslog message parser implementation in favor of the new 'syslogmp' package which was extracted from syslog2IRC. Bumped version to 0.8.

--- test_syslog2irc.py
+++ test_syslog2irc.py
@@ -35,10 +35,8 @@ module name!):
 .. _nose2: https://github.com/nose-devs/nose2
 
 
-:Copyright: 2007-2014 `Jochen Kupperschmidt <http://homework.nwsnet.de/>`_
-:Date: 19-May-2014 (original release: 12-Apr-2007)
+:Copyright: 2007-2015 `Jochen Kupperschmidt <http://homework.nwsnet.de/>`_
 :License: MIT, see LICENSE for details.
-:Version: 0.7
 """
 
 from __future__ import unicode_literals
@@ -46,11 +44,11 @@ from datetime import datetime
 from unittest import TestCase
 
 from nose2.tools import params
+from syslogmp import Facility, Message, Severity
 
-from syslog2irc import (IrcChannel, format_syslog_message,
-    irc_channel_joined, map_channel_names_to_ports,
-    parse_irc_server_arg, Processor, shutdown_requested, SyslogFacility,
-    SyslogMessage, SyslogMessageParser, SyslogSeverity)
+from syslog2irc import (IrcChannel, format_syslog_message, irc_channel_joined,
+    map_channel_names_to_ports, parse_irc_server_arg, Processor,
+    shutdown_requested)
 
 
 CURRENT_YEAR = datetime.today().year
@@ -60,99 +58,32 @@ class SyslogMessageTestCase(TestCase):
 
     @params(
         (
-            # Example 1 from RFC 3164.
-            '<34>Oct 11 22:14:15 mymachine su: \'su root\' failed for lonvick on /dev/pts/8',
-            SyslogFacility.security4,
-            'security/authorization messages',
-            SyslogSeverity.critical,
-            datetime(CURRENT_YEAR, 10, 11, 22, 14, 15),
-            'mymachine',
-            'su: \'su root\' failed for lonvick on /dev/pts/8',
-        ),
-        (
-            # Example 2 from RFC 3164.
-            '<13>Feb  5 17:32:18 10.0.0.99 Use the BFG!',
-            SyslogFacility.user,
-            'user-level messages',
-            SyslogSeverity.notice,
-            datetime(CURRENT_YEAR, 2, 5, 17, 32, 18),
-            '10.0.0.99',
-            'Use the BFG!',
-        ),
-        (
-            # Example 3 from RFC 3164.
-            # Note that the HOSTNAME and MSG fields in this example are not
-            # consistent with what the RFC defines.
-            '<165>Aug 24 05:34:00 CST 1987 mymachine myproc[10]: %% It\'s time to make the do-nuts.  %%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%',
-            SyslogFacility.local4,
-            'local use 4 (local4)',
-            SyslogSeverity.notice,
-            datetime(CURRENT_YEAR, 8, 24, 5, 34, 0),
-            'CST',
-            '1987 mymachine myproc[10]: %% It\'s time to make the do-nuts.  %%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%',
-        ),
-            # Example 4 from RFC 3164 (included for the sake of completeness).
-            # This cannot be parsed because the format of the TIMESTAMP field
-            # is invalid.
-            #'<0>1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That\'s All Folks!',
-        (
-            # Example 5 from RFC 3164.
-            '<0>Oct 22 10:52:12 scapegoat 1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That\'s All Folks!',
-            SyslogFacility.kernel,
-            'kernel messages',
-            SyslogSeverity.emergency,
-            datetime(CURRENT_YEAR, 10, 22, 10, 52, 12),
-            'scapegoat',
-            '1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That\'s All Folks!',
-        ),
-    )
-    def test_syslog_message_parser(
-            self,
-            data,
-            expected_facility,
-            expected_facility_description,
-            expected_severity,
-            expected_timestamp,
-            expected_hostname,
-            expected_message):
-        """Test parsing of a syslog message."""
-        actual = SyslogMessageParser.parse(data)
-
-        self.assertEqual(actual.facility, expected_facility)
-        self.assertEqual(actual.facility.description, expected_facility_description)
-        self.assertEqual(actual.severity, expected_severity)
-        self.assertEqual(actual.timestamp, expected_timestamp)
-        self.assertEqual(actual.hostname, expected_hostname)
-        self.assertEqual(actual.message, expected_message)
-
-    @params(
-        (
-            SyslogFacility.user,
-            SyslogSeverity.informational,
+            Facility.user,
+            Severity.informational,
             None,
             None,
             'FYI',
             '[informational]: FYI',
         ),
         (
-            SyslogFacility.clock9,
-            SyslogSeverity.warning,
+            Facility.clock9,
+            Severity.warning,
             datetime(2013, 7, 8, 0, 12, 55),
             None,
             'Tick, tack, watch the clock!',
             '[2013-07-08 00:12:55] [warning]: Tick, tack, watch the clock!',
         ),
         (
-            SyslogFacility.ntp,
-            SyslogSeverity.debug,
+            Facility.ntp,
+            Severity.debug,
             None,
             'ntp.local',
             'What time is it?',
             '(ntp.local) [debug]: What time is it?',
         ),
         (
-            SyslogFacility.kernel,
-            SyslogSeverity.emergency,
+            Facility.kernel,
+            Severity.emergency,
             datetime(2008, 10, 18, 17, 34, 7),
             'mainframe',
             'WTF? S.O.S.!',
@@ -162,8 +93,7 @@ class SyslogMessageTestCase(TestCase):
     def test_format_syslog_message(self, facility, severity, timestamp,
             hostname, message, expected):
         """Test string representation of a syslog message."""
-        message = SyslogMessage(facility, severity, timestamp, hostname,
-            message)
+        message = Message(facility, severity, timestamp, hostname, message)
 
         actual = format_syslog_message(message)
 
