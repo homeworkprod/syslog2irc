b34794dc0233ac124c16193afdcf33a8c1e54e2c
Jochen Kupperschmidt <homework@nwsnet.de>
2015-09-07 21:55:09 +0200

    Updated documentation on how to run tests, and moved it to the README.

--- test_syslog2irc.py
+++ test_syslog2irc.py
@@ -4,37 +4,6 @@
 Tests for syslog2IRC
 ====================
 
-
-Requirements
-------------
-
-- nose2_ (tested with version 0.4.6)
-
-
-Installation
-------------
-
-Install nose2_:
-
-.. code:: sh
-
-    $ pip install nose2
-
-
-Usage
------
-
-Run the tests (attention: do *not* specify the `.py` extension, just the
-module name!):
-
-.. code:: sh
-
-    $ nose2 test_syslog2irc
-
-
-.. _nose2: https://github.com/nose-devs/nose2
-
-
 :Copyright: 2007-2015 `Jochen Kupperschmidt <http://homework.nwsnet.de/>`_
 :License: MIT, see LICENSE for details.
 """
